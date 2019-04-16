078936eb251ae61b5a11a5b3a4a94283a7489a6c
norm warren <normpw@gmail.com>
2019-04-16 13:29:25 -0600

    done

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Switch, Route} from 'react-router-dom'
+import Post from './Components/Post/Post'
+
+import Home from './Components/Home/Home'
+import TopicList from './Components/TopicList/TopicList'
+
+export default (
+  <Switch>
+    <Route exact path='/' component={Home} />
+    <Route path='/topics' component={TopicList} />
+    <Route path='/post/:id' component={Post} />
+  </Switch>
+)
+
